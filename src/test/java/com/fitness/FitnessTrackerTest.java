2670482b873c144884360ab154e4aabead69c8c7
ahllyy <amanda.halley@keyin.com>
2025-02-07 21:07:49 -0330

    added testing for goal setting

--- src/test/java/com/fitness/FitnessTrackerTest.java
+++ src/test/java/com/fitness/FitnessTrackerTest.java
@@ -22,16 +22,16 @@ public class FitnessTrackerTest {
     @Test
     public void testWorkout() {
         List<Exercise> exercises = new ArrayList<>();
-        exercises.add(new Exercise("Shoulder Press",3, 12, 45));
-        exercises.add(new Exercise("Lateral Raise", 2, 15,20));
-        exercises.add(new Exercise("Rear Delt Fly", 2, 15,45));
+        exercises.add(new Exercise("Shoulder Press", 3, 12, 45));
+        exercises.add(new Exercise("Lateral Raise", 2, 15, 20));
+        exercises.add(new Exercise("Rear Delt Fly", 2, 15, 45));
 
         Workout workout = new Workout("Push Day", "60", "2025-02-02", exercises);
 
         assertEquals("Push Day", workout.getName());
-        assertEquals("60",workout.getTime());
+        assertEquals("60", workout.getTime());
         assertEquals("2025-02-02", workout.getDate());
-        assertEquals(3,workout.getExercise().size());
+        assertEquals(3, workout.getExercise().size());
         assertEquals("Shoulder Press", workout.getExercise().getFirst().getName());
 
     }
@@ -40,12 +40,20 @@ public class FitnessTrackerTest {
     @Test
     public void testLogWorkout() {
         WorkoutLog workoutLog = new WorkoutLog();
-        Workout workout  = new Workout("Push Day", "60", "2025-02-02", new ArrayList<>());
+        Workout workout = new Workout("Push Day", "60", "2025-02-02", new ArrayList<>());
 
         workoutLog.logWorkout(workout);
 
         assertEquals(1, workoutLog.getWorkouts().size());
         assertEquals("Push Day", workoutLog.getWorkouts().getFirst().getName());
+    }
+    //testing goalsetting
+    @Test
+    public void testGoalSetting() {
+        GoalSetting goalSetting = new GoalSetting();
+        goalSetting.setGoal("Shoulder press 45lbs");
+
+        assertEquals("Shoulder press 45lbs", goalSetting.getGoal());
     }
 }
 
