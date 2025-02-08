4787b6754934b480db3c28e7b969c141d90d5120
ahllyy <amanda.halley@keyin.com>
2025-02-07 21:08:44 -0330

    created progress tracker class for tracking users progress based on workouts loggged and goals set

--- src/main/java/com/fitness/ProgressTracker.java
+++ src/main/java/com/fitness/ProgressTracker.java
@@ -1,4 +1,24 @@
 package com.fitness;
 
 public class ProgressTracker {
+    private GoalSetting goal;
+
+    //constructor
+    public ProgressTracker(GoalSetting goal, WorkoutLog workoutLog) {
+        this.goal = goal;
+    }
+
+    public boolean checkProgressOfGoal() {
+        return goal.isGoalCompleted();          //confirms if goal is completed
+    }
+
+    public void currentGoalCompleted(){         //updates goal as complete
+        goal.completeGoal();
+    }
+
+    public String getGoal() {
+        return goal.getGoal();
+    }
+
+
 }
