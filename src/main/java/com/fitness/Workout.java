c053ceceee735990e07e51431e794000d4f75598
ahllyy <amanda.halley@keyin.com>
2025-02-07 20:53:52 -0330

    created workout class to store exercise details (name, date,time, exercises)

--- src/main/java/com/fitness/Workout.java
+++ src/main/java/com/fitness/Workout.java
@@ -1,4 +1,37 @@
 package com.fitness;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Workout {
+    private String name;
+    private String time;
+    private String date;
+    private List<Exercise> exercises;
+
+    //constructor
+    public Workout(String name, String time, String date, List<Exercise> exercises) {
+        this.name = name;
+        this.time = time;
+        this.date = date;
+        this.exercises = exercises;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    public String getTime() {
+        return time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public List<Exercise> getExercise() {
+
+        return Collections.unmodifiableList(exercises);
+    }
+
 }
