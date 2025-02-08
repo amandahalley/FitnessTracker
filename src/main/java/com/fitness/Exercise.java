6ab24226bf4d970b585bc57573d83d20e5a38bfd
ahllyy <amanda.halley@keyin.com>
2025-02-07 20:51:00 -0330

    created exercise class to log workout details (name, sets, reps, weight)

--- src/main/java/com/fitness/Exercise.java
+++ src/main/java/com/fitness/Exercise.java
@@ -1,4 +1,34 @@
 package com.fitness;
 
 public class Exercise {
+    private String name;
+    private int sets;
+    private int reps;
+    private int weight;
+
+    //constructor
+    public Exercise(String name, int sets, int reps, int weight) {
+        this.name = name;
+        this.sets = sets;
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
 }
