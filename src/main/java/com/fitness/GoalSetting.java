4f69980a962f46e84416eb9e0d0c9b5154855dfe
ahllyy <amanda.halley@keyin.com>
2025-02-07 21:04:52 -0330

    created class for creating goals for workouts

--- src/main/java/com/fitness/GoalSetting.java
+++ src/main/java/com/fitness/GoalSetting.java
@@ -1,4 +1,30 @@
 package com.fitness;
 
 public class GoalSetting {
+    private String goal;
+    private boolean goalCompleted;
+
+    //constructor
+    public GoalSetting() {
+        this.goal = "";
+        this.goalCompleted = false;  //sets goal to empty and if its complete false
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+        this.goalCompleted = false;  //resets for every new goal
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void completeGoal(){
+        this.goalCompleted = true;  //makes goal completed
+    }
+
+    public boolean isGoalCompleted() {
+        return goalCompleted;
+    }
+
 }
