20ed5bf3ba4a5327fe3eba1a2b3aacbfc8161d89
ahllyy <amanda.halley@keyin.com>
2025-02-07 21:02:55 -0330

    created workout log class to store and retrieve workouts created

--- src/main/java/com/fitness/WorkoutLog.java
+++ src/main/java/com/fitness/WorkoutLog.java
@@ -1,4 +1,23 @@
 package com.fitness;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorkoutLog {
+    private List<Workout> workouts;
+
+    //constructor
+    public WorkoutLog() {
+        this.workouts= new ArrayList<>();
+    }
+
+    //logging workout(includes exercises)
+    public void logWorkout(Workout workout) {
+        workouts.add(workout);
+    }
+
+    //get list of workouts logged
+    public List<Workout> getWorkouts() {
+        return workouts;
+    }
 }
