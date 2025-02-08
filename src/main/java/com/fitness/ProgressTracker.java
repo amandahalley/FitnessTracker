d8769b68d29986eb5f6aea909f70d40069fa86f7
ahllyy <amanda.halley@keyin.com>
2025-02-07 21:00:51 -0330

    removed tests as they were in the wrong class

--- src/main/java/com/fitness/ProgressTracker.java
+++ src/main/java/com/fitness/ProgressTracker.java
@@ -1,38 +1,4 @@
 package com.fitness;
 
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class ProgressTracker {
-
-    //testing Exercise class
-    @Test
-    public void testExercise() {
-        Exercise exercise = new Exercise("Shoulder Press", 3, 12, 45);
-        assertEquals(3, exercise.getSets());
-        assertEquals(12, exercise.getReps());
-        assertEquals(45, exercise.getWeight());
-    }
-
-    //testing workout class
-    @Test
-    public void testWorkout() {
-        List<Exercise> exercises = new ArrayList<>();
-        exercises.add(new Exercise("Shoulder Press",3, 12, 45));
-        exercises.add(new Exercise("Lateral Raise", 2, 15,20));
-        exercises.add(new Exercise("Rear Delt Fly", 2, 15,45));
-
-        Workout workout = new Workout("Push Day", "60", "2025-02-02", exercises);
-
-        assertEquals("Push Day", workout.getName());
-        assertEquals("60",workout.getTime());
-        assertEquals("2025-02-02", workout.getDate());
-        assertEquals(3,workout.getExercise().size());
-        assertEquals("Shoulder Press", workout.getExercise().getFirst().getName());
-
-    }
 }
